eea31bfe33adfdaf4d2d2d3243e981abadc46513
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2014-11-20 17:18:37 +0300

    Update docstrings

--- imgur-dl.py
+++ imgur-dl.py
@@ -5,10 +5,18 @@ import pyimgur
 
 
 class Imgur(object):
+    """An imgur (http://imgur.com) album downloader."""
 
     start_at = 0
 
     def __init__(self, client_id, username=None, album_id=None, start_at=0):
+        """Initialize the Imgur object.
+
+        :param client_id: imgur api client id
+        :param username: user account to download
+        :param album_id: album id to download
+        :param start_at: album/image to start at
+        """
         self.im = pyimgur.Imgur(client_id)
         self.start_at = start_at
         self.username = username if username else 'anonymous'
@@ -16,12 +24,14 @@ class Imgur(object):
         self.output = os.path.join('output', self.username)  # note self.username ref
 
     def dl(self):
+        """Download albums by user or album id."""
         if self.username is not 'anonymous':
             self.dl_username()
         elif self.album_id:
             self.dl_album_id()
 
     def dl_username(self):
+        """Download user albums."""
         # Keep the limit high, default's to 100
         albums = self.im.get_user(self.username).get_albums(limit=1000)
         for index, album in enumerate(albums[self.start_at:], self.start_at):
@@ -30,6 +40,7 @@ class Imgur(object):
                 self.image_download(image, ix + 1, len(images))
 
     def dl_album_id(self):
+        """Download album by id."""
         albums = [self.im.get_album(self.album_id)]
         for index, album in enumerate(albums):
             images = self.process_album(album, index + 1, len(albums))
@@ -39,10 +50,15 @@ class Imgur(object):
     def process_album(self, album, count, total):
         album_name = '%02d - %s - %s' % (count, album.id.lower(), album.title)
         print '(%d/%d) Processing %s' % (count, total, album_name)
-        self.check_path(album_name)
+        self.makedirs(album_name)
+        self.make_dirs(album_name)
         return self.im.get_album(album.id).images
 
-    def check_path(self, album_name):
+    def make_dirs(self, album_name):
+        """Create the download directory.
+
+        :param album_name: album name
+        """
         self.album_dir = os.path.join(self.output, album_name)
         if not os.path.exists(self.album_dir):
             os.makedirs(self.album_dir)
@@ -55,9 +71,9 @@ class Imgur(object):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Download albums from imgur.')
+    parser = argparse.ArgumentParser(description='Download public albums from imgur.')
     parser.add_argument('client_id', metavar='client-id',  help='imgur api client id')
-    parser.add_argument('-u', '--username', help='user account to download (public albums)')
+    parser.add_argument('-u', '--username', help='user account to download (albums)')
     parser.add_argument('-a', '--album-id', help='album id to download')
     parser.add_argument('-s', '--start-at', type=int, default=0, help='album/image to start at (resume)')
     args = parser.parse_args()
